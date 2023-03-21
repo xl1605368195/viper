049a68d12492e588dafb031972c9425bc4370da3
-1day <1605368195@qq.com>
2023-03-21 16:30:59 +0800

    Update viper.go

--- viper.go
+++ viper.go
@@ -40,14 +40,14 @@ import (
 	"github.com/spf13/cast"
 	"github.com/spf13/pflag"
 
-	"github.com/spf13/viper/internal/encoding"
-	"github.com/spf13/viper/internal/encoding/dotenv"
-	"github.com/spf13/viper/internal/encoding/hcl"
-	"github.com/spf13/viper/internal/encoding/ini"
-	"github.com/spf13/viper/internal/encoding/javaproperties"
-	"github.com/spf13/viper/internal/encoding/json"
-	"github.com/spf13/viper/internal/encoding/toml"
-	"github.com/spf13/viper/internal/encoding/yaml"
+	"github.com/xl1605368195/viper/internal/encoding"
+	"github.com/xl1605368195/viper/internal/encoding/dotenv"
+	"github.com/xl1605368195/viper/internal/encoding/hcl"
+	"github.com/xl1605368195/viper/internal/encoding/ini"
+	"github.com/xl1605368195/viper/internal/encoding/javaproperties"
+	"github.com/xl1605368195/viper/internal/encoding/json"
+	"github.com/xl1605368195/viper/internal/encoding/toml"
+	"github.com/xl1605368195/viper/internal/encoding/yaml"
 )
 
 // ConfigMarshalError happens when failing to marshal the configuration.
