de9573b05440a46688f5905d2bba2fa1f1652921
-1day <1605368195@qq.com>
2023-03-21 16:31:31 +0800

    Update util_test.go

--- util_test.go
+++ util_test.go
@@ -16,7 +16,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/spf13/viper/internal/testutil"
+	"github.com/xl1605368195/viper/internal/testutil"
 )
 
 func TestCopyAndInsensitiviseMap(t *testing.T) {
